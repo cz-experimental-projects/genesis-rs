7479b2e3eff2c50a6bc5219c221b125f639af41f
W1nter-IsHere <w1nter.ish3re@gmail.com>
2023-03-25 10:20:26 -0400

    More plugins'

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_prototype_lyon::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(Msaa::Sample4)
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(ShapePlugin)
+        .add_startup_system(setup_system)
         .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::RegularPolygon {
+        sides: 6,
+        feature: shapes::RegularPolygonFeature::Radius(200.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shape),
+            ..default()
+        },
+        Fill::color(Color::CYAN),
+        Stroke::new(Color::BLACK, 10.0),
+    ));
 }
